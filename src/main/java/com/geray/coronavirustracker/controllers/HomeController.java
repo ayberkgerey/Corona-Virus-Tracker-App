8a8d9dfddaed3c68bc90b115e1344d6d8dc0f5cf
Ayberk Gerey <ayberkgerey@gmail.com>
2020-03-01 00:30:56 +0300

    Add files via upload

--- src/main/java/com/geray/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/geray/coronavirustracker/controllers/HomeController.java
@@ -1,12 +1,22 @@
 package com.geray.coronavirustracker.controllers;
 
+import com.geray.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    public String home(){
-        return"";
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        //access with this
+        model.addAttribute("locationStats" , coronaVirusDataService.getAllStats);
+        return "home";
     }
 
 }
