a46fde72eb70cb6ffd355e04f8dab3f496fbb3fc
Ayberk Gerey <ayberkgerey@gmail.com>
2020-02-29 21:10:20 +0300

    Add files via upload

---
+++ src/test/java/com/geray/coronavirustracker/CoronaVirusTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.geray.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusTrackerApplication {
+
+    public static void main(String[] args){ SpringApplication.run(CoronaVirusTrackerApplication.class); }
+
+
+}
